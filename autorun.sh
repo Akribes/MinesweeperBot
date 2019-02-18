3b5b38c86ce992fa6addaed746ca8f5eb1e8c702
Mart Koster <the947thtokay@codingtime.nl>
2019-02-18 17:11:41 +0100

    Update autorun script to execute correct file and compile after pulling

--- autorun.sh
+++ autorun.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-mvn package
 while :
 do
     git pull
-    java -jar target/
+    mvn install
+    java -jar "Minesweeper Bot.jar"
     echo "Press Ctrl+C now to stop! Program will start in 10 seconds."
     sleep 10
 done
