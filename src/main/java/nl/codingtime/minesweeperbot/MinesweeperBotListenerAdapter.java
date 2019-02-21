1f88f5349032c99d77a1135c5b9be17f33b20727
Mart Koster <the947thtokay@codingtime.nl>
2019-02-21 17:19:54 +0100

    Clean up listener adapter and add information for the bot to send

--- src/main/java/nl/codingtime/minesweeperbot/MinesweeperBotListenerAdapter.java
+++ src/main/java/nl/codingtime/minesweeperbot/MinesweeperBotListenerAdapter.java
@@ -1,5 +1,9 @@
 package nl.codingtime.minesweeperbot;
 
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.MessageEmbed;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
@@ -11,6 +15,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MinesweeperBotListenerAdapter extends ListenerAdapter {
+    private static final String GETTING_STARTED_MESSAGE = "Hi, I'm MinesweeperBot. I can generate Minesweeper puzzles" +
+            " for you! To get started, DM or ping me the size and amount of mines for the puzzle. For example, if you" +
+            " say `5/6/7`, I'll send you a puzzle of 5 by 6 squares with 7 mines. If you're completely new to" +
+            " Minesweeper, read *How to play it?*, or you might get blown up by a mine!";
+    private static final String HOW_TO_PLAY_MESSAGE = "A Minesweeper puzzle is a grid of squares you can click on." +
+            " On this grid, several mines are located. The goal is to click all squares except for the squares with" +
+            " mines. When you click on a square, one of these symbols is revealed:\n" +
+            "- Nothing: this means that all eight squares around the square you clicked don't have mines.\n" +
+            "- A number: this means that in the eight surrounding squares, that number of mines are hidden.\n" +
+            "- A mine: game over!\n" +
+            "There should be very little guessing involved. Think carefully!";
+    private static final String INVITE_MESSAGE = "If you want me to, I can be in your server too! Here's an" +
+            "authorization link: ";
+    private static final String STATS_MESSAGE = "I keep track of what I'm doing. Here are some statistics about me:\n" +
+            "I've hidden **%mines% mines** in **%puzzles% puzzles** for **%users% users** in **%guilds% servers** and" +
+            " **%privateChannels% private channels.**";
     private MinesweeperBot bot;
 
     public MinesweeperBotListenerAdapter(MinesweeperBot bot) {
@@ -27,23 +47,43 @@ public class MinesweeperBotListenerAdapter extends ListenerAdapter {
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
         if (event.getAuthor().isBot()) return;
         if (event.getMessage().getContentRaw().startsWith("<@" + bot.getJda().getSelfUser().getId() + ">")) {
-            generateMessages(event.getMessage().getContentRaw(), event.getAuthor()).forEach(message -> event.getChannel().sendMessage(
-                    message).queue());
+            handleCommand(event.getMessage().getContentRaw(), event.getAuthor(), event.getChannel());
         }
     }
 
     @Override
     public void onPrivateMessageReceived(PrivateMessageReceivedEvent event) {
         if (event.getAuthor().isBot()) return;
-        generateMessages(event.getMessage().getContentRaw(), event.getAuthor()).forEach(message -> event.getChannel().sendMessage(
-                message).queue());
+        handleCommand(event.getMessage().getContentRaw(), event.getAuthor(), event.getChannel());
     }
 
-    private List<String> generateMessages(String message, User requestingUser) {
+    private void handleCommand(String message, User sender, MessageChannel channel) {
         String command =  message.replace("<@" + bot.getJda().getSelfUser().getId() + ">", "").trim();
         String[] puzzle = command.split("/");
-        String result = "%ping%, make sure to follow this format: `width/height/mines` (all positive numbers)!";
-        if (Integer.parseInt(puzzle[0]) * Integer.parseInt(puzzle[1]) > bot.getConfig().getMaxSize()) {
+        String result;
+        if (!command.replaceFirst("/", "").contains("/")) {
+            channel.sendMessage(new EmbedBuilder().setTitle("About MinesweeperBot")
+                    .setDescription("Here's some information about me!")
+                    .setColor(0xFFA901)
+                    .setThumbnail(bot.getJda().getSelfUser().getAvatarUrl())
+                    .addField("Let's play Minesweeper!", GETTING_STARTED_MESSAGE, false)
+                    .addField("How to play it?", HOW_TO_PLAY_MESSAGE, false)
+                    .addField("May I blow up your server?",
+                            INVITE_MESSAGE + bot.getJda().asBot().getInviteUrl(
+                                    Permission.VIEW_CHANNEL, Permission.MESSAGE_READ, Permission.MESSAGE_WRITE,
+                                    Permission.MESSAGE_EXT_EMOJI, Permission.MESSAGE_HISTORY,
+                                    Permission.MESSAGE_ADD_REACTION), false)
+                    .addField("Stats", STATS_MESSAGE
+                            .replaceAll("%mines%", String.valueOf(bot.getStats().getMines()))
+                            .replaceAll("%puzzles%", String.valueOf(bot.getStats().getUses()))
+                            .replaceAll("%users%", String.valueOf(bot.getStats().getUserIds().size()))
+                            .replaceAll("%guilds%", String.valueOf(bot.getStats().getServers()))
+                            .replaceAll("%privateChannels%", String.valueOf(bot.getStats().getPrivateChannels())),
+                            false)
+                    .setFooter("Made by Mart#1056.", bot.getJda().getUserById(350609220846223362L).getAvatarUrl())
+                    .build()).queue();
+            return;
+        } else if (Integer.parseInt(puzzle[0]) * Integer.parseInt(puzzle[1]) > bot.getConfig().getMaxSize()) {
             result = "%ping%, the puzzle you want me to generate will be too big!";
         } else {
             try {
@@ -52,7 +92,7 @@ public class MinesweeperBotListenerAdapter extends ListenerAdapter {
                 bot.getStats().incrementUses();
                 bot.getStats().incrementMines(Long.parseLong(puzzle[2]));
             } catch (IllegalArgumentException | ArrayIndexOutOfBoundsException e) {
-                // Nothing wrong, just tell the user that he made a mistake
+                result = "%ping%, make sure to follow this format: `width/height/mines` (all positive numbers)!";
             }
         }
 
@@ -75,11 +115,13 @@ public class MinesweeperBotListenerAdapter extends ListenerAdapter {
 
         for (int i = 0; i < messages.size(); i++) {
             String sentMessage = messages.get(i);
-            messages.set(i, sentMessage.replace("%ping%", "<@" + requestingUser.getId() + ">"));
+            messages.set(i, sentMessage.replace("%ping%", "<@" + sender.getId() + ">"));
         }
 
-        bot.getStats().registerUser(requestingUser.getIdLong());
+        bot.getStats().registerUser(sender.getIdLong());
 
-        return messages;
+        for (String s : messages) {
+            channel.sendMessage(s).queue();
+        }
     }
 }
